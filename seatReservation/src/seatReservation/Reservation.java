16c40b1584ee79ae1d4d7185144bc986a937005a
haeun Noh <okhagrace06@naver.com>
2023-02-13 15:51:36 +0900

    초반 실행 메뉴 출력

--- seatReservation/src/seatReservation/Reservation.java
+++ seatReservation/src/seatReservation/Reservation.java
@@ -1,7 +1,23 @@
 package seatReservation;
 
+import java.util.Scanner;
+
 public class Reservation {
-	public static void main(String[] args) {
-		
+	
+	Scanner scan = new Scanner(System.in);
+
+	// 초반 실행할 때 메뉴
+	void run() {
+		while(true) {
+			System.out.println("=======**********========");
+			System.out.println("=======* 좌석예약 *========");
+			System.out.println("=======**********========");
+			this.selectMenu();
+		}
+	}
+	
+	int selectMenu() {
+		System.out.println("예약[1]    조회[2]    취소[3]    종료[4]  >> ");
+		return scan.nextInt();
 	}
 }
