41c0fec1457d2d87b9ada17dd257a6f36adb7756
haeun Noh <okhagrace06@naver.com>
2023-02-14 09:26:26 +0900

    [예약] 이름,자리번호 입력 후 예약 완료

--- seatReservation/src/seatReservation/Reservation.java
+++ seatReservation/src/seatReservation/Reservation.java
@@ -2,22 +2,60 @@ package seatReservation;
 
 import java.util.Scanner;
 
-public class Reservation {
-	
-	Scanner scan = new Scanner(System.in);
+public class Reservation extends CheckSeat {
 
-	// 초반 실행할 때 메뉴
-	void run() {
-		while(true) {
-			System.out.println("=======**********========");
-			System.out.println("=======* 좌석예약 *========");
-			System.out.println("=======**********========");
-			this.selectMenu();
+	Scanner scan = new Scanner(System.in);
+	
+	String userName;
+	int seatNum;
+
+	public void run() {
+		System.out.print("[좌석구분] S[1] A[2] B[3] >> ");
+		int seatNum = scan.nextInt();
+		switch (seatNum) {
+		case 1:
+			checkSeatS();
+			reserSeatS();
+			break;
+		case 2:
+			checkSeatA();
+			reserSeatA();
+			break;
+		case 3:
+			checkSeatB();
+			reserSeatB();
+			break;
+		default:
+			System.out.println("좌석을 잘못 선택했습니다.");
 		}
 	}
+
+	// 예약
+	public void reserSeatS() {
+		initNameToSelectSeat();
+		initNumToSelectSeat();
+		seat_S[seatNum-1] = this.userName;
+	}
+
+	public void reserSeatA() {
+		initNameToSelectSeat();
+		initNumToSelectSeat();
+		seat_A[seatNum-1] = this.userName;
+	}
+
+	public void reserSeatB() {
+		initNameToSelectSeat();
+		initNumToSelectSeat();
+		seat_B[seatNum-1] = this.userName;
+	}
 	
-	int selectMenu() {
-		System.out.println("예약[1]    조회[2]    취소[3]    종료[4]  >> ");
-		return scan.nextInt();
+	// 이름, 번호 예약
+	public void initNameToSelectSeat() {
+		System.out.print("이름 >> ");
+		this.userName = scan.next();
+	}
+	public void initNumToSelectSeat() {
+		System.out.print("번호 >> ");
+		this.seatNum = scan.nextInt();
 	}
 }
