d607e5dd82df985a219bd370284a61469c542d6a
haeun Noh <okhagrace06@naver.com>
2023-02-14 09:52:44 +0900

    [예약] 중복 예약 불가능하게 해야함

--- seatReservation/src/seatReservation/Reservation.java
+++ seatReservation/src/seatReservation/Reservation.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Reservation extends CheckSeat {
 
 	Scanner scan = new Scanner(System.in);
-	
+
 	String userName;
 	int seatNum;
 
 	public void run() {
 		System.out.print("[좌석구분] S[1] A[2] B[3] >> ");
-		int seatNum = scan.nextInt();
-		switch (seatNum) {
+		int seatClass = scan.nextInt();
+
+		initNameToSelectSeat();
+		initNumToSelectSeat();
+
+		switch (seatClass) {
 		case 1:
 			checkSeatS();
 			reserSeatS();
@@ -26,34 +30,43 @@ public class Reservation extends CheckSeat {
 			reserSeatB();
 			break;
 		default:
-			System.out.println("좌석을 잘못 선택했습니다.");
+			System.out.println("<<< 좌석을 잘못 선택했습니다. >>>");
 		}
 	}
 
 	// 예약
+	/*
+	 * TODO: 중복 처리 불가능하게
+	 */
 	public void reserSeatS() {
-		initNameToSelectSeat();
-		initNumToSelectSeat();
-		seat_S[seatNum-1] = this.userName;
+		System.out.println("S랭크입니다.");
+		if (!seat_S[seatNum - 1].equals("----")) {
+			seat_S[seatNum - 1] = this.userName;
+			System.out.println("if문 안 들어감");
+		} else
+			System.out.println("<<< 이미 예약된 좌석입니다. >>>");
 	}
 
 	public void reserSeatA() {
-		initNameToSelectSeat();
-		initNumToSelectSeat();
-		seat_A[seatNum-1] = this.userName;
+		if (!seat_A[seatNum - 1].equals("----"))
+			seat_A[seatNum - 1] = this.userName;
+		else
+			System.out.println("<<< 이미 예약된 좌석입니다. >>>");
 	}
 
 	public void reserSeatB() {
-		initNameToSelectSeat();
-		initNumToSelectSeat();
-		seat_B[seatNum-1] = this.userName;
+		if (!seat_B[seatNum - 1].equals("----"))
+			seat_B[seatNum - 1] = this.userName;
+		else
+			System.out.println("<<< 이미 예약된 좌석입니다. >>>");
 	}
-	
-	// 이름, 번호 예약
+
+	// 이름, 좌석 번호 예약
 	public void initNameToSelectSeat() {
 		System.out.print("이름 >> ");
 		this.userName = scan.next();
 	}
+
 	public void initNumToSelectSeat() {
 		System.out.print("번호 >> ");
 		this.seatNum = scan.nextInt();
