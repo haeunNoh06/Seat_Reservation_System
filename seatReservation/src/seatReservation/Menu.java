e8a4f316aa0f1694f8b4244dc3b364957513f5fe
haeun Noh <okhagrace06@naver.com>
2023-02-15 14:43:35 +0900

    [메뉴] 예약 취소 메서드 추가

--- seatReservation/src/seatReservation/Menu.java
+++ seatReservation/src/seatReservation/Menu.java
@@ -9,28 +9,34 @@ public class Menu {
 	Reservation reservation = new Reservation();// 예약
 	CheckSeat checkSeat = new CheckSeat();// 조회
 	CancelSeat cancelSeat = new CancelSeat();// 취소
+	
+	boolean isEnded = false;
 
 	void run() {
 		System.out.println("▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝ ▜");
 		System.out.print("▘        ");
 		System.out.print("좌석예약");
 		System.out.println("         ▝");
-		System.out.print("▙▗▗▗▗▗▗▗▗▗▗▗▗▗▗▗▗▗▗ ▟\n");
-		while (true) {
+		System.out.print("▙▗▗▗▗▗▗▗▗▗▗▗▗▗▗▗▗▗▗ ▟\n\n");
+		
+		checkSeat.run();
+		
+		while ( !isEnded ) {
 			this.selectMenu();
 		}
+		System.out.println("<<< 좌석 예약이 종료되었습니다. >>>");
 	}
 
-	int selectMenu() {
+	void selectMenu() {
 		System.out.print("\n예약[1]    조회[2]    취소[3]    종료[4]  >> ");
 		int menuNum = scan.nextInt();
+		System.out.println();
 		switch(menuNum) {
-			case 1: // 예약
+			case 1: reservation.run(); break;
 			case 2: checkSeat.run(); break;
-			case 3: // 취소
-			case 4: // 종료
-			default: System.out.println("잘못 입력하셨습니다.");
+			case 3: cancelSeat.run(); break;
+			case 4: this.isEnded = true; break;
+			default: System.out.println("<<< 다시 입력하세요. >>>");
 		}
-		return 0;
 	}
 }
